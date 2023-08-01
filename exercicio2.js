3be20f5a62611cf545adab25a40df90f5565909f
jaays-lima <117986211+jaays-lima@users.noreply.github.com>
2023-08-01 08:31:02 -0300

    Add files

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+let num = Number(prompt("Informe um numero"));
+let conta =num*3;
+alert(`O resultado da multiplicação por 3 é: ${conta}`)
+var resposta = prompt("Deseja Continuar?")
+while(resposta=="sim"||resposta=="SIM"){
+let num = Number(prompt("Informe um número"));
+let conta = num*3;
+alert(`O resultado da multiplicação por 3 é ${conta}`)
+var reposta = prompt("Deseja continuar?")
+}
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+let 
+function adicionar(){
+ let txt = document.querySelector('#txt').value;
+
+ let arraytext=[];
+
+ arraytext.push(txt);
+ 
+ console.log(arraytxt);
+}
+
+
+
+
